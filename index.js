324c760e716298f069caf90b5828e7942952755d
EvertonLDS815 <evertonlino1234@gmail.com>
2023-08-18 15:51:38 -0300

    feat: No altered mongoose

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-require("dotenv").config();
-const connetToDb = require("./database/db");
+require('dotenv').config();
+const connetToDb = require('./database/db');
 
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 500;
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-const No = require("./model/No");
+const No = require('./model/No');
 
 connetToDb();
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   const knots = await No.find();
   res.json(knots);
 });
 
-app.post("/ins", async (req, res) => {
+app.post('/ins', async (req, res) => {
   const newKnots = req.body;
-  await KNonots.create(newKnots);
+  await No.create(newKnots);
 
   res.status(201).json(newKnots);
 });
-app.put("/ins/:id", async (req, res) => {
+app.put('/ins/:id', async (req, res) => {
   const { id } = req.params;
   const { name, nivel, type, linkImage, linkVideo, description } = req.body;
   await No.findByIdAndUpdate(id, {
@@ -37,7 +37,7 @@ app.put("/ins/:id", async (req, res) => {
 
   res.sendStatus(204);
 });
-app.delete("/ins/:id", async (req, res) => {
+app.delete('/ins/:id', async (req, res) => {
   const { id } = req.params;
   await No.findByIdAndDelete(id);
 
